86f129d4745c5b728d2bee1e25e4b8d7f82a8fe9
Basile Simon <basile@basilesimon.fr>
2021-05-21 15:34:33 +0200

    Analytics on Fathom

--- content/js/analytics.js
+++ content/js/analytics.js
@@ -9,3 +9,15 @@
     var d=document, g=d.createElement('script'), s=d.getElementsByTagName('script')[0];
     g.type='text/javascript'; g.async=true; g.src=u+'matomo.js'; s.parentNode.insertBefore(g,s);
   })();
+
+(function(f, a, t, h, o, m){
+	a[h]=a[h]||function(){
+		(a[h].q=a[h].q||[]).push(arguments)
+	};
+	o=f.createElement('script'),
+	m=f.getElementsByTagName('script')[0];
+	o.async=1; o.src=t; o.id='fathom-script';
+	m.parentNode.insertBefore(o,m)
+})(document, window, '//fathom.basilesimon.fr/tracker.js', 'fathom');
+fathom('set', 'siteId', 'EEOUL');
+fathom('trackPageview');
